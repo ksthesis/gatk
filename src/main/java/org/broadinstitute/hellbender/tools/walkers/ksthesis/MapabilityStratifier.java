8b069ffeab5aa5e1f82807f9f0e3eed131120436
Khalid Shakir <kshakir@broadinstitute.org>
2017-07-15 13:10:06 -0400

    Stratifers now in a generic list, and adding zero rows in walker.

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/ksthesis/MapabilityStratifier.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/ksthesis/MapabilityStratifier.java
@@ -5,6 +5,7 @@ import org.broadinstitute.hellbender.engine.FeatureInput;
 
 import java.util.List;
 
+@SuppressWarnings("WeakerAccess")
 public class MapabilityStratifier extends FeatureStratifier<BEDFeature> {
 
     private final int binSize;
